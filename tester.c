8e341272cff3b6ef94510efa58e79dea136e1d74
Letitia Li <letitia.li@telecom-paristech.fr>
2016-10-24 13:32:02 +0200

    Replace tester.c with 4 motors and magnet

--- tester.c
+++ tester.c
@@ -40,6 +40,7 @@ int main( void )
 	uint8_t sn_color;
 	uint8_t sn_compass;
 	uint8_t sn_sonar;
+	uint8_t sn_mag;
 	char s[ 256 ];
 	int val;
 	float value;
@@ -69,6 +70,7 @@ int main( void )
 			printf( "  port = %s\n", ev3_tacho_port_name( i, s ));
 		}
 	}
+	//Note that you may need to change this code depending on the order and type of your motors
 	if ( ev3_search_tacho( LEGO_EV3_M_MOTOR, &sn, 0 )) {
 		int max_speed;
 
@@ -156,6 +158,35 @@ int main( void )
 	} else {
 		printf( "LEGO_EV3_L_MOTOR 3 is NOT found\n" );
 	}
+	if ( ev3_search_tacho( LEGO_EV3_L_MOTOR, &sn, 3 )) {
+		int max_speed;
+
+		printf( "LEGO_EV3_L_MOTOR 4 is found, run for 5 sec...\n" );
+		get_tacho_max_speed( sn, &max_speed );
+		printf("  max_speed = %d\n", max_speed );
+		set_tacho_stop_action_inx( sn, TACHO_COAST );
+		set_tacho_speed_sp( sn, max_speed * 2 / 3 );
+		set_tacho_time_sp( sn, 5000 );
+		set_tacho_ramp_up_sp( sn, 2000 );
+		set_tacho_ramp_down_sp( sn, 2000 );
+		set_tacho_command_inx( sn, TACHO_RUN_TIMED );
+		/* Wait tacho stop */
+		Sleep( 100 );
+		do {
+			get_tacho_state_flags( sn, &state );
+		} while ( state );
+		printf( "run to relative position...\n" );
+		set_tacho_speed_sp( sn, max_speed / 2 );
+		set_tacho_ramp_up_sp( sn, 0 );
+		set_tacho_ramp_down_sp( sn, 0 );
+		set_tacho_position_sp( sn, 90 );
+		for ( i = 0; i < 8; i++ ) {
+			set_tacho_command_inx( sn, TACHO_RUN_TO_REL_POS );
+			Sleep( 500 );
+		}
+	} else {
+		printf( "LEGO_EV3_L_MOTOR 4 is NOT found\n" );
+	}
 //Run all sensors
 	ev3_sensor_init();
 
@@ -199,6 +230,14 @@ int main( void )
 		if (ev3_search_sensor(LEGO_EV3_US, &sn_sonar,0)){
 			printf("SONAR found, reading sonar...\n");
 			if ( !get_sensor_value0(sn_sonar, &value )) {
+				value = 0;
+			}
+			printf( "\r(%f) \n", value);
+			fflush( stdout );
+	    	}
+		if (ev3_search_sensor(NXT_ANALOG, &sn_mag,0)){
+			printf("Magnetic sensor found, reading magnet...\n");
+			if ( !get_sensor_value0(sn_mag, &value )) {
 				value = 0;
 			}
 			printf( "\r(%f) \n", value);
