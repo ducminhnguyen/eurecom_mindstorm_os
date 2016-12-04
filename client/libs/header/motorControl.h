9e5460dca7fc9c0f5847b44a0d52bcc2c720c340
nmduc <nmduc@apcs.vn>
2016-12-04 14:41:42 +0100

    Add headers

---
+++ client/libs/header/motorControl.h
@@ -0,0 +1,12 @@
+//
+// Created by parallels on 12/4/16.
+//
+
+#ifndef OS_ROBOT_PROJECT_FALL2016_MOTORCONTROL_H
+#define OS_ROBOT_PROJECT_FALL2016_MOTORCONTROL_H
+
+void runStraight();
+void turnLeft(double degree);
+void turnRight(double degree);
+
+#endif //OS_ROBOT_PROJECT_FALL2016_MOTORCONTROL_H
---
+++ client/libs/movement/motorControl.c
@@ -0,0 +1,5 @@
+//
+// Created by parallels on 12/4/16.
+//
+#include "../header/motorControl.h"
+
