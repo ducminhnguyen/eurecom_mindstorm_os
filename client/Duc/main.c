b0a553290256c83072473e32d816d19a60c73f3b
nmduc <nmduc@apcs.vn>
2016-12-12 13:02:51 +0100

    dasca

--- client/Duc/main.c
+++ client/Duc/main.c
@@ -86,7 +86,7 @@ void UpdateSensorInfo(struct SensorInfo* info) { // update
     }
     info->currentColor = getColorSensorValue(*info);
     if (info->currentColor < 15 && info->currentColor > 0) {
-        usleep(1000);
+        sleep(1);
         robotState = ROBOT_STOP;
     }
     return;
