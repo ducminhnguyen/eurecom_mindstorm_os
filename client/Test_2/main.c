74b5a6a130bd208fb5b7accac7c92091e6981c45
Nguyen Quang Viet Hoang <nguyenqv@eurecom.fr>
2016-12-13 10:35:32 +0100

    update test 2

--- client/Test_2/main.c
+++ client/Test_2/main.c
@@ -233,7 +233,7 @@ int main( void ) {
     b_motor_info.speed = -500;
     b_motor_info.time = 1000;
     b_motor_info.command = TACHO_RUN_TIMED;
-    StartRunning(motorInfo);
+    StartRunning(b_motor_info);
     runStraightLine(b_motor_info, info);
     sleep(1);
     stopRobot(b_motor_info);
