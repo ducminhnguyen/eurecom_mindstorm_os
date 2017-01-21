b82b6024bee1cefb58a922fbf7d65204e9a671ad
nmduc <nmduc@apcs.vn>
2017-01-21 18:22:32 +0100

    dasca

--- client/Step_Model/main.c
+++ client/Step_Model/main.c
@@ -66,6 +66,7 @@ void init_robot_steps(MotorInfo* motorInfo, SensorInfo* sensorInfo) {
     global_params.robot_steps[0].robot_run_straight_until_wall_distance_to_stop = 30;
     global_params.robot_steps[0].robot_run_timed_time_to_run = 2;
     global_params.robot_steps[0].robot_run_timed_speed = -400;
+    global_params.robot_steps[0].robot_turn_left_degree = 90;
     global_params.robot_steps[0].init_step = &robotturnleft_init_step;
     global_params.robot_steps[0].run_motor = &robotturnleft_run_motor;
     global_params.robot_steps[0].update_all = &robotturnleft_update;
