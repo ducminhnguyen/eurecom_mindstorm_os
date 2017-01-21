3cd1155463d6a10b35c54712b5adcc600162fe99
nmduc <nmduc@apcs.vn>
2017-01-21 21:12:38 +0100

    dasca

--- client/libs/movement/robotruntimedstep.c
+++ client/libs/movement/robotruntimedstep.c
@@ -28,14 +28,14 @@ void robotruntimed_run_motor(MotorInfo *motorInfo, SensorInfo *sensorInfo) {
     get_tacho_speed(motorInfo->rightMotor, &right_motor_speed);
 
     if (global_params.robot_state == ROBOT_RUN_STRAIGHT) {
-        printf("mult: %d\n", (motorInfo->speed > 0)*2 - 1);
+        //printf("mult: %d\n", (motorInfo->speed > 0)*2 - 1);
         if (sensorInfo->diffGyro > 0) {
-            set_tacho_speed_sp(motorInfo->rightMotor, motorInfo->speed - 10*((motorInfo->speed > 0)*2 - 1)*abs(sensorInfo->diffGyro));
+            set_tacho_speed_sp(motorInfo->rightMotor, motorInfo->speed - 10*abs(sensorInfo->diffGyro));
             set_tacho_speed_sp(motorInfo->leftMotor, motorInfo->speed);
             printf("Left tilt: %f \n", sensorInfo->diffGyro);
         } else if (sensorInfo->diffGyro < 0) {
             set_tacho_speed_sp(motorInfo->rightMotor, motorInfo->speed);
-            set_tacho_speed_sp(motorInfo->leftMotor, motorInfo->speed - 10*((motorInfo->speed > 0)*2 - 1) *abs(sensorInfo->diffGyro));
+            set_tacho_speed_sp(motorInfo->leftMotor, motorInfo->speed - 10*abs(sensorInfo->diffGyro));
             printf("Right tilt: %f \n", sensorInfo->diffGyro);
         } else {
             set_tacho_speed_sp(motorInfo->leftMotor, motorInfo->speed);
