5b55ad54c63036dd1c203126e6437981fa2b2fcb
nmduc <nmduc@apcs.vn>
2017-01-21 19:26:11 +0100

    dasca

---
+++ client/libs/movement/robotgrabballstep.c
@@ -0,0 +1,6 @@
+//
+// Created by parallels on 1/22/17.
+//
+
+#include "../header/std_include.h"
+#include "../header/robotgrabballstep.h"
--- client/libs/movement/robotturnleftstep.c
+++ client/libs/movement/robotturnleftstep.c
@@ -10,7 +10,7 @@ void robotturnleft_update(MotorInfo *motorInfo, SensorInfo *sensorInfo) {
     update_sensor_value(sensorInfo);
     if (global_params.robot_state == ROBOT_TURN_LEFT) {
         //printf("update: %d\n", abs(abs(sensorInfo->diffGyro) - global_params.robot_steps[global_params.current_step].robot_turn_left_degree));
-        if(abs(abs(sensorInfo->diffGyro) - (global_params.robot_steps[global_params.current_step].robot_turn_left_degree)) < 10){
+        if(abs(abs(sensorInfo->diffGyro) - (global_params.robot_steps[global_params.current_step].robot_turn_left_degree)) < 3){
             global_params.robot_state = ROBOT_STOP_RUNNING;
         }
     } else if (global_params.robot_state == ROBOT_STOP_RUNNING) {
