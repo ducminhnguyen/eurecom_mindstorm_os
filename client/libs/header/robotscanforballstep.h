d89a242feb75006ca7538a0cb697d2871595c1d9
nmduc <nmduc@apcs.vn>
2017-01-21 21:17:30 +0100

    dasca

--- client/libs/header/robotscanforballstep.h
+++ client/libs/header/robotscanforballstep.h
@@ -9,15 +9,15 @@
 
 
 // call this function in the update all function in the loop
-void robotscanforballstep_update(MotorInfo *motorInfo, SensorInfo *sensorInfo);
+void robotscanforball_update(MotorInfo *motorInfo, SensorInfo *sensorInfo);
 
 
 // call this function in the run motor function of the loop
-void robotscanforballstep_run_motor(MotorInfo *motorInfo, SensorInfo *sensorInfo);
+void robotscanforball_run_motor(MotorInfo *motorInfo, SensorInfo *sensorInfo);
 
 // this function is called whenever the state is entered to ensure starting parameter of a state is always in the
 // right form. Implement this function if you think at the start of this step sensor and motor need to be in a specific
 // state
-void robotscanforballstep_init_step(MotorInfo *motorInfo, SensorInfo *sensorInfo);
+void robotscanforball_init_step(MotorInfo *motorInfo, SensorInfo *sensorInfo);
 
 #endif //OS_ROBOT_PROJECT_FALL2016_ROBOTSCANFORBALLSTEP_H
