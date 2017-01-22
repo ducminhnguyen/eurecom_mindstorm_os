5f0fd8e2ac58d50797b754c04ed70ff18704cd17
Nguyen Quang Viet Hoang <nguyenqv@eurecom.fr>
2017-01-22 15:54:38 +0100

    asdad

--- client/libs/movement/global.c
+++ client/libs/movement/global.c
@@ -4,7 +4,9 @@
 
 #include "../header/global.h"
 #include <time.h>
+#include <math.h>
 
+#define PI 3.14159265
 
 int movetonextstep(global_parameters *global_param, MotorInfo* motorInfo, SensorInfo *sensorInfo) {
     if (global_param->run_style == LOOP) {
@@ -28,25 +30,24 @@ int movetonextstep(global_parameters *global_param, MotorInfo* motorInfo, Sensor
 int update_robot_position(global_parameters *global_param, MotorInfo* motorInfo, SensorInfo* sensorInfo) 
 {
     static time_t previous_updated_time = 0;
+    time_t current_time = time(NULL);
 
     if (global_param->robot_state == ROBOT_RUN_STRAIGHT) 
     {
         // update position based-on gyro and ultra sonic sensor
+        float d_x, x_y;
+        d_x = cos(PI * ((sensorInfo->currentGyro - global_param->robot_direction_x) % 360) );
 
     }
     
-    if (previous_updated_time == 0) // update distance every
+
+    if (current_time - previous_updated_time >= 2)
     {
         /* code */
+        // send bluetooth position to server
+        SendRobotPosition(global_param);
+        previous_updated_time = current_time;
     }
-    else 
-    {
-        if (time(NULL) - previous_updated_time >= 2)
-        {
-            /* code */
-            // send bluetooth position to server
-        }
-        previous_updated_time = time(NULL);
-    }
+    
 
 }
