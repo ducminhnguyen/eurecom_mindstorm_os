a60d8eb02d2b10922ec9cff95ec6ee62e0c54d96
nmduc <nmduc@apcs.vn>
2017-01-22 17:26:53 +0100

    dasca

--- client/libs/movement/robotgrabballstep.c
+++ client/libs/movement/robotgrabballstep.c
@@ -70,5 +70,5 @@ void robotgrabball_run_motor(MotorInfo *motorInfo, SensorInfo *sensorInfo) {
 void robotgrabball_init_step(MotorInfo *motorInfo, SensorInfo *sensorInfo) {
     set_sensor_initial_values(sensorInfo);
     global_params.robot_state = ROBOT_OPEN_GRABBER;
-
+    global_params.robot_steps[global_params.current_step].robot_run_timed_time_to_run = 1000.0f;
 }
