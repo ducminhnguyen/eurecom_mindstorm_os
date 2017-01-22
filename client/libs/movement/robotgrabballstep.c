e743a6daf4575d009af9520ffb44e6abc525c120
nmduc <nmduc@apcs.vn>
2017-01-22 17:59:55 +0100

    dasca

--- client/libs/movement/robotgrabballstep.c
+++ client/libs/movement/robotgrabballstep.c
@@ -30,9 +30,9 @@ void robotgrabball_update(MotorInfo *motorInfo, SensorInfo *sensorInfo) {
 void robotgrabball_run_motor(MotorInfo *motorInfo, SensorInfo *sensorInfo) {
     if (global_params.robot_state == ROBOT_OPEN_GRABBER) {
         set_tacho_speed_sp(motorInfo->graberMotor, -GRABBER_SPEED);
-        set_tacho_time_sp(motorInfo->graberMotor, 700);
-        set_tacho_command_inx(motorInfo->graberMotor, TACHO_RUN_FOREVER);
-        usleep(GRABBER_TIME * 1000);
+        set_tacho_time_sp(motorInfo->graberMotor, 2000);
+        set_tacho_command_inx(motorInfo->graberMotor, TACHO_RUN_TIMED);
+        usleep(2000 * 1000);
         global_params.robot_state = ROBOT_RUN_STRAIGHT;
         begin_time = clock();
     } else if (global_params.robot_state == ROBOT_RUN_STRAIGHT) {
@@ -59,9 +59,9 @@ void robotgrabball_run_motor(MotorInfo *motorInfo, SensorInfo *sensorInfo) {
         set_tacho_command_inx(motorInfo->leftMotor, TACHO_STOP);
         set_tacho_command_inx(motorInfo->rightMotor, TACHO_STOP);
 
-        //set_tacho_speed_sp(motorInfo->graberMotor, GRABBER_SPEED);
-        //set_tacho_time_sp(motorInfo->graberMotor, GRABBER_TIME);
-        //set_tacho_command_inx(motorInfo->graberMotor, TACHO_RUN_TIMED);
+        set_tacho_speed_sp(motorInfo->graberMotor, GRABBER_SPEED);
+        set_tacho_time_sp(motorInfo->graberMotor, GRABBER_TIME);
+        set_tacho_command_inx(motorInfo->graberMotor, TACHO_RUN_TIMED);
         usleep(GRABBER_TIME * 1000);
         global_params.robot_state = ROBOT_STOP_RUNNING;
     }
