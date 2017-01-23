8d775f8c0d5626fb05ffc6ae027df1451b8f652f
Nguyen Quang Viet Hoang <nguyenqv@eurecom.fr>
2017-01-23 14:26:20 +0100

    commit release ball

--- client/libs/movement/robotreleaseballstep.c
+++ client/libs/movement/robotreleaseballstep.c
@@ -72,7 +72,7 @@ void robotreleaseball_run_motor(MotorInfo *motorInfo, SensorInfo *sensorInfo) {
         set_tacho_speed_sp(motorInfo->graberMotor, GRABBER_SPEED);
         set_tacho_time_sp(motorInfo->graberMotor, GRABBER_TIME);
         set_tacho_command_inx(motorInfo->graberMotor, TACHO_RUN_TIMED);
-        global_params.robot_state = ROBOT_STOP_RUNNING;
+        
     } else if (global_params.robot_state == ROBOT_STOP_RUNNING) {
         set_tacho_speed_sp(motorInfo->leftMotor, 0);
         set_tacho_speed_sp(motorInfo->rightMotor, 0);
@@ -80,6 +80,13 @@ void robotreleaseball_run_motor(MotorInfo *motorInfo, SensorInfo *sensorInfo) {
         set_tacho_command_inx(motorInfo->graberMotor, TACHO_STOP);
         set_tacho_command_inx(motorInfo->leftMotor, TACHO_STOP);
         set_tacho_command_inx(motorInfo->rightMotor, TACHO_STOP);
+    } else if (global_params.robot_state == ROBOT_COMPLETE_STEP) {
+        set_tacho_speed_sp(motorInfo->leftMotor, 0);
+        set_tacho_speed_sp(motorInfo->rightMotor, 0);
+        set_tacho_speed_sp(motorInfo->graberMotor, 0);
+        set_tacho_command_inx(motorInfo->graberMotor, TACHO_RESET);
+        set_tacho_command_inx(motorInfo->leftMotor, TACHO_RESET);
+        set_tacho_command_inx(motorInfo->rightMotor, TACHO_RESET);
     }
 }
 
