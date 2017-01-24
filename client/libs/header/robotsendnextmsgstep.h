bd20fec6dd7a7fdaa96d9bd06ebdc71bbb584be3
nmduc <nmduc@apcs.vn>
2017-01-24 16:45:41 +0100

    dasca

--- client/libs/header/robotsendnextmsgstep.h
+++ client/libs/header/robotsendnextmsgstep.h
@@ -1,2 +1,17 @@
-84e10290c49989bf510ce032d0d456083b069c9b		branch 'master' of https://gitlab.eurecom.fr/ludovic.apvrille/OS_Robot_Project_Fall2016
-1c6fd76b9b8eca76ca089dcff1b41394d8ebe620	not-for-merge	branch 'graphics' of https://gitlab.eurecom.fr/ludovic.apvrille/OS_Robot_Project_Fall2016
+//
+// Created by DuongLam on 1/23/2017.
+//
+
+#ifndef ROBOT_BUNBO_ROBOTSENDNEXTMSGSTEP_H
+#define ROBOT_BUNBO_ROBOTSENDNEXTMSGSTEP_H
+
+// call this function in the update all function in the loop
+void robotsendnextmsg_update(MotorInfo *motorInfo, SensorInfo *sensorInfo);
+// call this function in the run motor function of the loop
+void robotsendnextmsg_run_motor(MotorInfo *motorInfo, SensorInfo *sensorInfo);
+// this function is called whenever the state is entered to ensure starting parameter of a state is always in the
+// right form. Implement this function if you think at the start of this step sensor and motor need to be in a specific
+// state
+void robotsendnextmsg_init_step(MotorInfo *motorInfo, SensorInfo *sensorInfo);
+
+#endif //ROBOT_BUNBO_ROBOTSENDNEXTMSGSTEP_H
